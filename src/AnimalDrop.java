6aff0d72a13b08babb0c151d04fcda2f19fb06a9
Lukasz <leszman.lukasz@gmail.com>
2023-03-29 01:14:56 +0200

    project to drop random animal cards made one JPanel and basics for further development

--- src/AnimalDrop.java
+++ src/AnimalDrop.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class AnimalDrop {
     String[][] NumberOfCards;
     int drawNumber;
@@ -8,4 +12,29 @@ public class AnimalDrop {
 
     String s = Integer.toString(rand);
 
+    private JButton JBInfo;
+    private JButton JBPlay;
+    private JPanel JPMainPanel;
+
+    public AnimalDrop() {
+        JBPlay.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //przycisk aby przzeszlo do opcji losowania kart
+                // oraz pokazywania kart
+            }
+        });
+        JBInfo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            //przycisk aby pokazywal liczbe mozliwych losowan
+            //oraz inne wartosci
+            }
+        });
+
+
+    }
+    JPanel getPanelGlowny() {
+        return JPMainPanel;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
+    AnimalDrop animaldrop = new AnimalDrop();
+        JFrame okienko = new JFrame("Animal Drop");
+        okienko.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        okienko.setSize(500, 200);
+        okienko.setContentPane(animaldrop.getPanelGlowny());
 
 
 
@@ -11,8 +18,7 @@ public class Main {
 
 
 
-
-
+// checking how multidimensional array works
 //        String[][] NumberOfCards = {
 //                {"1", "2"},
 //                {"5", "6"},
